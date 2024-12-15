4675713f4833b1a28a3fa8443f19c75badd01ea4
bazraatamir <lhagvasuren.bazarrragchaa89@gmail.com>
2024-12-16 02:22:41 +0800

    first

---
+++ src/Overlay.js
@@ -0,0 +1,10 @@
+export default function Overlay({inside, setInside}) {
+  return (
+    <>
+      <header>
+        <img draggable={false} width='100%' src='/amka-white.png' />
+      </header>
+      <footer className='footer'></footer>
+    </>
+  );
+}
