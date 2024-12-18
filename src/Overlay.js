dae8af4e208b896978a9b116eb5f543258685f02
saruul-890 <saruulzaya7337@gmail.com>
2024-12-18 13:13:57 +0800

    Update Overlay.js

--- src/Overlay.js
+++ src/Overlay.js
@@ -2,7 +2,6 @@ export default function Overlay({inside, setInside}) {
   return (
     <>
       <header>
-        <img draggable={false} width='100%' src='/amka-white.png' />
       </header>
       <footer className='footer'></footer>
     </>
